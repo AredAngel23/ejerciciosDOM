4ec475a428ec9a84e3148a269c4425e1b3190fca
Ared <ared230000@gmail.com>
2024-10-16 13:06:37 -0600

    03 Dibujo Canvas

--- dibujo.js/app.js
+++ dibujo.js/app.js
@@ -23,30 +23,81 @@ ctx.lineTo(105, 130);
 ctx.fill();
 
 */
-
+// Calabza (Circulo)
 ctx.fillStyle = 'orange';
 ctx.beginPath();
 ctx.arc(290, 200, 120, 0, Math.PI * 2, true);
 ctx.fill();
 
+// Líneas de la calabaza (Ovalos)
+ctx.strokeStyle = 'grey';
+ctx.beginPath();
+ctx.ellipse(290, 200, 10, 120, 0, 0, Math.PI * 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.ellipse(290, 200, 50, 120, 0, 0, Math.PI * 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.ellipse(290, 200, 90, 120, 0, 0, Math.PI * 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.ellipse(290, 200, 120, 120, 0, 0, Math.PI * 2);
+ctx.stroke();
+
+// OJOS (Triangulos)
 ctx.fillStyle = 'black';
 ctx.beginPath();
-ctx.moveTo(220, 140);
-ctx.lineTo(270, 140);
-ctx.lineTo(245, 180);
+ctx.moveTo(245, 132);
+ctx.lineTo(270, 172);
+ctx.lineTo(220, 172);
 ctx.fill();
 
 ctx.fillStyle = 'black';
 ctx.beginPath();
-ctx.moveTo(310, 140);
-ctx.lineTo(360, 140);
-ctx.lineTo(335, 180);
+ctx.moveTo(345, 132);
+ctx.lineTo(370, 172);
+ctx.lineTo(320, 172);
 ctx.fill();
 
+// NARIZ (Triangulo)
 ctx.fillStyle = 'black';
 ctx.beginPath();
-ctx.moveTo(265, 200);
-ctx.lineTo(315, 200);
-ctx.lineTo(290, 240);
+ctx.moveTo(265, 188);
+ctx.lineTo(315, 188);
+ctx.lineTo(290, 228);
 ctx.fill();
 
+// BOCA (Cuadrados)
+ctx.fillRect(255, 248, 70, 30);
+
+ctx.fillRect(226, 230, 30, 30);
+ctx.fillRect(324, 230, 30, 30); 
+
+ctx.fillRect(196, 210, 30, 30);
+ctx.fillRect(354, 210, 30, 30);
+
+// TOP (Cuadrados)
+ctx.fillStyle = 'green';
+ctx.fillRect(255, 83, 70, 35);
+ctx.fillRect(268, 70, 45, 30);
+
+// Telarañas (Líneas)
+ctx.strokeStyle = 'grey';
+ctx.beginPath();
+ctx.moveTo(70, 0);
+ctx.lineTo(70, 130);
+
+ctx.moveTo(0, 60);
+ctx.lineTo(150, 60);
+
+ctx.moveTo(5, 5);
+ctx.lineTo(135, 115);
+
+ctx.moveTo(145, 5);
+ctx.lineTo(0, 115);
+
+ctx.lineWidth = 3;
+ctx.stroke();
